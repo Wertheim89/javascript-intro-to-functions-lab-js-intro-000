8f9a8307628826cf3dd16a5a3817702376d4f5aa
Thomas Varo <zalipsn@gmail.com>
2019-07-20 13:42:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,3 +22,11 @@ function sayHiToGrandma(string) {
         return  "I can't hear you!";
     }
 }
+
+function sayHiToGrandma(string) {
+    if (string === string.toUpperCase()) {
+        return  "YES INDEED!";
+    }
+}
+
+
